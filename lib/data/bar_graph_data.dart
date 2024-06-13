b0968f67435c95da949832d8510c45f4424b24d4
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-13 07:15:44 +0700

    update

--- lib/data/bar_graph_data.dart
+++ lib/data/bar_graph_data.dart
@@ -6,32 +6,23 @@ class BarGraphData {
   final data = [
     const BarGraphModel(label: "April revenue", color: Color(0xFFFEB95A), graph: [
       GraphModel(x: 0, y: 8),
-      GraphModel(x: 2, y: 7),
+      GraphModel(x: 1, y: 3),
       GraphModel(x: 2, y: 7),
 
     ]),
     const BarGraphModel(label: "May revenue", color: Color(0xFFF2C8ED), graph: [
       GraphModel(x: 0, y: 8),
-      GraphModel(x: 3, y: 6),
-      GraphModel(x: 4, y: 6),
+      GraphModel(x: 1, y: 6),
+      GraphModel(x: 2, y: 3),
 
     ]),
     const BarGraphModel(label: "June revenue", color: Color(0xFF20AEF3), graph: [
       GraphModel(x: 0, y: 7),
       GraphModel(x: 1, y: 10),
-      GraphModel(x: 2, y: 7),
+      GraphModel(x: 2, y: 2),
 
     ]),
   ];
-
-  // final label = [
-  //   'H',
-  //   'K',
-  //   'T',
-  //   'A',
-  //   'H',
-  //   'N',
-  // ];
 
   final label = [
     'God Father',
